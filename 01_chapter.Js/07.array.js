290b2e16afc71deefba939ecfaf82af2f403c943
Megha rajak <mmegharajak98@gmail.com>
2026-07-05 22:23:36 +0530

    Initial JavaScript practice

---
+++ 01_chapter.Js/07.array.js
@@ -0,0 +1,11 @@
+// let student = "aman";
+// let student = "megha";
+//let student = "sachin";
+
+let student = ["aman", "megha", "sachin"];
+
+let fruits = [ 'mango', 'apple', 'litchi'];
+console.log(fruits[0]);
+
+
+let cars = ['bmw', 'audi', 'mercedes', 'xuv'];
