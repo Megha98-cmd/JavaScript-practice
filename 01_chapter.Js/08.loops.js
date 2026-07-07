2c3e41b8b676083d456bc46f2cc326579a72b4df
Megha rajak <mmegharajak98@gmail.com>
2026-07-08 01:18:19 +0530

    Initial JavaScript practice

--- 01_chapter.Js/08.loops.js
+++ 01_chapter.Js/08.loops.js
@@ -32,9 +32,47 @@ for(let i =5; i <= 50; i=i+5) {
     console.log(i);
 }
    
-let n = prompt("write your number");
-n = parseInt(n);
+//let n = prompt("write your number");
+//n = parseInt(n);
 
-for(let i=n; i<=10*n; i=i+n) {
-    console.log(i);
-}
+//for(let i=n; i<=10*n; i=i+n) {
+  //  console.log(i);
+//}
+
+
+//Favorite Movie
+//const favoriteMovie = "Pritam and Petro";
+
+//let guess = prompt("Guess my favorite movie");
+
+//while( (guess != favoriteMovie) && (guess != "Quite") ) {
+  //  guess = prompt("Wrong Guess! Please try again");
+//}
+
+//if (guess == favoriteMovie) {
+ //   console.log("You guessed it right!");
+//}
+//else {
+   // console.log("You gave up!");
+//}
+
+//Loop with Array
+//let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+//for (let i = 0; i < fruits.length; i++) {
+//    console.log(i,fruits[i]);
+//}
+
+//Nested Loop
+let heroes = [
+            ["Ironman", "Spiderman", "Batman", "Superman"],
+            ["Wonder Woman", "Black Widow", "Carol Danvers", "Storm"]
+         ]
+
+         for (let i=0; i < heroes.length; i++) {
+            console.log(i, heroes[i], heroes[i].length);
+            for (let j = 0; j < heroes[i].length; j++) {
+                console.log(`j=${j}, ${heroes[i][j]}`);
+            }
+        }
+    
