c9b40503036fafea32bb6548b9c237278d9624bb
Megha rajak <mmegharajak98@gmail.com>
2026-07-20 23:31:08 +0530

    Initial JavaScript practice

--- 02_chapter.Js/02_arrow function.js
+++ 02_chapter.Js/02_arrow function.js
@@ -1,7 +1,29 @@
+
+//Arrow function
 const sum = (a, b) => {
     console.log(a + b);
 };
 
 const cube = (n) => {
-    return(n * n * n);
+    return n * n * n;
 };
+
+//Arrow function with implicit return
+const mul = (a, b) => a * b;
+
+
+
+//setTimeout
+console.log("Hii there!");
+
+setTimeout(() => {
+    console.log("coding");
+}, 2000);
+
+console.log("Welcome to");
+
+//setInterval 
+
+//setInterval( () => {
+ //   console.log("Megha");
+//}, 1000 );
