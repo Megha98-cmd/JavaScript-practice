2b0c3688d933ef4912b3df70f99d618ebda995b5
Megha rajak <mmegharajak98@gmail.com>
2026-07-28 18:55:36 +0530

    Initial JavaScript practice

---
+++ 02_chapter.Js/03_array method.js
@@ -0,0 +1,21 @@
+
+//Array Method
+let arr = [
+{
+    name: "aman",
+    marks: 94.4,
+},
+{
+    name: "megha",
+    marks: 90.9,
+},
+{
+    name: "rajat",
+    marks: 85,
+},
+
+];
+
+arr .forEach((student) => {
+    console.log(student.marks);
+});
