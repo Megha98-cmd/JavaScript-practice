4e0d5ac6e8565002e46117bd0411e599c7a4bd9d
Megha rajak <mmegharajak98@gmail.com>
2026-07-28 18:26:03 +0530

    Initial JavaScript practice

--- 02_chapter.Js/02_arrow function.js
+++ 02_chapter.Js/02_arrow function.js
@@ -13,19 +13,19 @@ const mul = (a, b) => a * b;
 
 
 
-//setTimeout
-//console.log("Hii there!");
+setTimeout
+console.log("Hii there!");
 
-//setTimeout(() => {
-  //  console.log("coding");
-//}, 2000);
+setTimeout(() => {
+    console.log("coding");
+}, 2000);
 
-//console.log("Welcome to");
+console.log("Welcome to");
 
-//setInterval 
+setInterval 
 
-//setInterval( () => {
-  //  console.log("Megha");
-//}, 1000 );
+setInterval( () => {
+    console.log("Megha");
+}, 1000 );
 
 
