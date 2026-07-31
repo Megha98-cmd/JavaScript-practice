151709b670233a2d8e6175ba5e6fd9764566606c
Megha rajak <mmegharajak98@gmail.com>
2026-07-31 14:58:39 +0530

    Initial JavaScript practice

--- 02_chapter.Js/03_array method.js
+++ 02_chapter.Js/03_array method.js
@@ -26,3 +26,18 @@ let num = [1, 2, 3, 4, 5];
 let double = num.map((el) => {
     return el * el;
 });
+
+//Filter Method
+//let nums = [1, 2, 3, 4, 5, 7, 9, 10, 11];
+//let ans = nums.filter((el) => {
+  //  return !(el % 2 == 0);
+
+    //});
+
+    //Reduce Method
+    let nums = [1, 2, 3, 4];
+    let finalVal = nums. reduce((res, el) => {
+        console.log( res);
+        return res + el;
+    });
+    console.log(finalVal);
