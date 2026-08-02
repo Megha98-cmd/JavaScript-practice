0f29d43a0811f408edbe6810a9567e1bb98c1d4b
Megha rajak <mmegharajak98@gmail.com>
2026-08-02 22:09:02 +0530

    Initial JavaScript practice

--- 02_chapter.Js/03_array method.js
+++ 02_chapter.Js/03_array method.js
@@ -1,24 +1,24 @@
 
 //Array Method
-let arr = [
-{
-    name: "aman",
-    marks: 94.4,
-},
-{
-    name: "megha",
-    marks: 90.9,
-},
-{
-    name: "rajat",
-    marks: 85,
-},
+//let arr = [
+//{
+  //  name: "aman",
+    //marks: 94.4,
+//},
+//{
+  //  name: "megha",
+    //marks: 90.9,
+//},
+//{
+  //  name: "rajat",
+    //marks: 85,
+//},
 
-];
+//];
 
-arr .forEach((student) => {
-    console.log(student.marks);
-});
+//arr .forEach((student) => {
+  //  console.log(student.marks);
+//});
 
 //Map Method
 let num = [1, 2, 3, 4, 5];
@@ -28,11 +28,11 @@ let double = num.map((el) => {
 });
 
 //Filter Method
-let nums = [1, 2, 3, 4, 5, 7, 9, 10, 11];
-let ans = nums.filter((el) => {
-    return !(el % 2 == 0);
+//let nums = [1, 2, 3, 4, 5, 7, 9, 10, 11];
+//let ans = nums.filter((el) => {
+  //  return !(el % 2 == 0);
 
-    });
+    //});
 
     //Reduce Method
     let nums = [1, 2, 3, 4];
@@ -41,3 +41,13 @@ let ans = nums.filter((el) => {
         return res + el;
     });
     console.log(finalVal);
+
+
+    let arr = [1, 2, 3, 4, 5, 9, 10, 11];
+    let max = -1;
+    for (let i = 0; i < arr.length; i++) {
+        if ( max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    console.log(max);
