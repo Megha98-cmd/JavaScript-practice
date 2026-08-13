ffa532c9f4db2a35d615defd52ab8d3415d73197
Megha rajak <mmegharajak98@gmail.com>
2026-08-13 23:47:35 +0530

    Initial JavaScript practice

--- 02_chapter.Js/04_default parameters.js
+++ 02_chapter.Js/04_default parameters.js
@@ -1,8 +1,9 @@
-//function sum(a = 2, b) {
-    //return a + b;
-//}
+function sum(a = 2, b = 3) {
+    return a + b;
+}
 
-//sum(3, 5); // returns 8
-
+sum(3, 5); // returns 8
+sum(3); // returns 6
+sum(); // returns 5
 
 
