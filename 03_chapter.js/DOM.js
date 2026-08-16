39c9bfc4b3ac6ff80275174c59ce94727bc6d922
Megha rajak <mmegharajak98@gmail.com>
2026-08-17 01:00:19 +0530

    Initial JavaScript practice

--- 03_chapter.js/DOM.js
+++ 03_chapter.js/DOM.js
@@ -1,5 +1,12 @@
 let smallImages = document.getElementsByClassName("oldImg");
 
-for (let i=0; i < smallImages.length; i++) {
+for (let i = 0; i < smallImages.length; i++) {
     console.dir(smallImages[i].src);
 }
+
+
+console.dir(document.querySelector("h1"));
+
+console.dir(document.querySelector("#description"));
+
+console.dir(document.querySelector(".oldname"));
