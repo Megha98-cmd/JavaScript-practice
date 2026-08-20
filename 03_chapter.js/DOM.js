1041a3fab74304280a7773c6b1ecedfb1abb0432
Megha rajak <mmegharajak98@gmail.com>
2026-08-20 23:59:34 +0530

    Initial JavaScript practice

--- 03_chapter.js/DOM.js
+++ 03_chapter.js/DOM.js
@@ -5,8 +5,8 @@ for (let i = 0; i < smallImages.length; i++) {
 }
 
 
-//console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("h1"));
 
-//console.dir(document.querySelector("#description"));
+console.dir(document.querySelector("#description"));
 
-//console.dir(document.querySelector(".oldname"));
+console.dir(document.querySelector(".oldname"));
