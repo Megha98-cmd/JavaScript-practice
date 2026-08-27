159ed550de2bc8b31f4be6629a8b83c41a43a3f4
Megha rajak <mmegharajak98@gmail.com>
2026-08-27 11:49:49 +0530

    Initial JavaScript practice

--- 03_chapter.js/practice ques.js
+++ 03_chapter.js/practice ques.js
@@ -6,20 +6,20 @@ para1.classList.add('red');
 
 
 
-//let h3 =document.createElement('h3');
-//h3.innerText = "I'm a blue h3!";
-//document.body.appendChild(h3);
-//h3.classList.add('blue');
+let h3 =document.createElement('h3');
+h3.innerText = "I'm a blue h3!";
+document.body.appendChild(h3);
+h3.classList.add('blue');
 
 
-//let div = document.createElement("div");
-//let h1 = document.createElement("h1");
-//let para2 = document.createElement("p");
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
 
-//h1.innerText = "I'm in a div";
-//para2.innerText = "ME TOO!";
+h1.innerText = "I'm in a div";
+para2.innerText = "ME TOO!";
 
-//div.appendChild(h1);
-//div.appendChild(para2);
-//div.classList.add('box');
-//document.body.appendChild(div);
+div.appendChild(h1);
+div.appendChild(para2);
+div.classList.add('box');
+document.body.appendChild(div);
