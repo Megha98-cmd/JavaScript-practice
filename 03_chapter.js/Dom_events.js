bc5dc41846bdcfe5bc05d01d418fa103d16050e0
Megha rajak <mmegharajak98@gmail.com>
2026-09-04 02:06:30 +0530

    Initial JavaScript practice

--- 03_chapter.js/Dom_events.js
+++ 03_chapter.js/Dom_events.js
@@ -1,25 +1,47 @@
-let btns = document.querySelectorAll("button");
+//let btns = document.querySelectorAll("button");
 //console.dir(btn);
 
 //btn.onclick = function() {
 //  console.log("button was clicked");
 //};
 
-for (btn of btns) {
+//for (btn of btns) {
    // btn.onclick = sayHello;
    // btn.onmouseenter = function() {
       //  btn.addEventListener("click", sayHello);
        // btn.addEventListener("click", sayName);
-         btn.addEventListener("dblclick", function () {
-            console.log(" You Double Clicked me");
-        });
-    }  
+         //btn.addEventListener("dblclick", function () {
+          //  console.log(" You Double Clicked me");
+       // });
+   // }  
     
 
-function sayHello() {
-    alert("Hello World");
-}
+//function sayHello() {
+ //   alert("Hello World");
+//}
 
-function sayName(){
-    alert("My name is Megha");
+//function sayName(){
+ //   alert("My name is Megha");
+//}
+
+
+//Activity
+
+let  btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+
+    console.log("Color updated");
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
 }
