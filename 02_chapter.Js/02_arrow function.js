55e240030f9eb7bfe1eb4cc4e676c9879ebf73c4
Megha rajak <mmegharajak98@gmail.com>
2026-09-06 02:04:47 +0530

    Initial JavaScript practice

--- 02_chapter.Js/02_arrow function.js
+++ 02_chapter.Js/02_arrow function.js
@@ -1,12 +1,12 @@
 
 //Arrow function
-//const sum = (a, b) => {
-  //  console.log(a + b);
-//};
+const sum = (a, b) => {
+    console.log(a + b);
+};
 
-//const cube = (n) => {
-//    return n * n * n;
-//};
+const cube = (n) => {
+    return n * n * n;
+};
 
 //Arrow function with implicit return
 const mul = (a, b) => a * b;
