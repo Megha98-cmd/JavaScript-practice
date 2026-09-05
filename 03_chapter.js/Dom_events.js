ddd421c862f984785b37106c5071b2f9f39ba302
Megha rajak <mmegharajak98@gmail.com>
2026-09-05 23:06:37 +0530

    Initial JavaScript practice

--- 03_chapter.js/Dom_events.js
+++ 03_chapter.js/Dom_events.js
@@ -1,29 +1,29 @@
-let btns = document.querySelectorAll("button");
-console.dir(btn);
+//let btns = document.querySelectorAll("button");
+//console.dir(btn);
 
-btn.onclick = function() {
-  console.log("button was clicked");
-};
+//btn.onclick = function() {
+//  console.log("button was clicked");
+//};
 
-for (btn of btns) {
-    btn.onclick = sayHello;
-    btn.onmouseenter = function() {
-        btn.addEventListener("click", sayHello);
-        btn.addEventListener("click", sayName);
-         btn.addEventListener("dblclick", function () {
-            console.log(" You Double Clicked me");
-        });
-    }  
+//for (btn of btns) {
+//    btn.onclick = sayHello;
+  //  btn.onmouseenter = function() {
+  //    btn.addEventListener("click", sayHello);
+    //    btn.addEventListener("click", sayName);
+      //   btn.addEventListener("dblclick", function () {
+        //    console.log(" You Double Clicked me");
+       // });
+   // }  
     
 
-function sayHello() {
-    alert("Hello World");
-}
+//function sayHello() {
+  //  alert("Hello World");
+//}
 
-function sayName(){
-    alert("My name is Megha");
-}
-  }
+//function sayName(){
+  //  alert("My name is Megha");
+//}
+  //}
 
 
 //Activity
