505187eb9d473a8f790ff4058350b4bc9d489e1c
Megha rajak <mmegharajak98@gmail.com>
2026-09-05 22:57:51 +0530

    Initial JavaScript practice

--- 03_chapter.js/Dom_events.js
+++ 03_chapter.js/Dom_events.js
@@ -1,19 +1,19 @@
-//let btns = document.querySelectorAll("button");
-//console.dir(btn);
+let btns = document.querySelectorAll("button");
+console.dir(btn);
 
-//btn.onclick = function() {
-//  console.log("button was clicked");
-//};
+btn.onclick = function() {
+  console.log("button was clicked");
+};
 
-//for (btn of btns) {
-   // btn.onclick = sayHello;
+for (btn of btns) {
+    btn.onclick = sayHello;
    // btn.onmouseenter = function() {
       //  btn.addEventListener("click", sayHello);
        // btn.addEventListener("click", sayName);
          //btn.addEventListener("dblclick", function () {
           //  console.log(" You Double Clicked me");
        // });
-   // }  
+    }  
     
 
 //function sayHello() {
