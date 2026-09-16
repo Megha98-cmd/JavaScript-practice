5d77c18e58e48463f70f1d9f1eb15e254d5bd14f
Megha rajak <mmegharajak98@gmail.com>
2026-09-17 01:24:13 +0530

    Initial JavaScript practice

--- 04_chapter.js/form_event.js
+++ 04_chapter.js/form_event.js
@@ -3,19 +3,19 @@ let form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     
-//    let username = document.getElementById("username");
-//    let password = document.getElementById("password");
-//    console.log(username.value);
-//    console.log(password.value);
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    console.log(username.value);
+    console.log(password.value);
 
 
-//    alert(`Hi ${username.value}, your password is ${password.value}`);
+    alert(`Hi ${username.value}, your password is ${password.value}`);
 });
 
 
-let user = document.querySelector("#user");
+//let user = document.querySelector("#user");
 
-user.addEventListener("change", function (){
-    console.log("input changed");
-    console.log("final value = ", this.value);
-});
+//user.addEventListener("change", function (){
+//    console.log("input changed");
+//    console.log("final value = ", this.value);
+//});
